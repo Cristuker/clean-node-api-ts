3f77472b7e68c7cbf23429970d58e2e93d8ecf6e
Cristian Silva <cristian123105@gmail.com>
2020-07-12 17:24:23 -0300

    feat: signUp Controller added

---
+++ src/presentation/controllers/signup.ts
@@ -0,0 +1,7 @@
+/* eslint-disable class-methods-use-this */
+export class SignUpController {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  handle(httpRequest: any): any {
+    return { statusCode: 400 };
+  }
+}
