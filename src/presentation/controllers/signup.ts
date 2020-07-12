6f2f059a7788c6b98c7aed0160240e7a6500f644
Cristian Silva <cristian123105@gmail.com>
2020-07-12 17:49:51 -0300

    feat: return error in body

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,6 +2,9 @@
 export class SignUpController {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   handle(httpRequest: any): any {
-    return { statusCode: 400 };
+    return {
+      statusCode: 400,
+      body: new Error('Missing param: name'),
+    };
   }
 }
