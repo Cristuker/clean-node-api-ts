7bd86cb372e6e3326a5f9a7c37616afbe2c3c8a5
Cristian Silva <cristian123105@gmail.com>
2020-07-14 10:25:56 -0300

    test: ensure return 500 when email validar throws an error

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -2,7 +2,7 @@ import { SignUpController } from './signup';
 import { MissingParamError } from '../erros/missing-param-error';
 import { InvalidParamError } from '../erros/invalid-param-error';
 import { EmailValidator } from '../protocols/email-validator';
-
+import { ServerError } from '../erros/server-error'
 interface SutTypes {
   sut: SignUpController,
   emailValidatorStb: EmailValidator
@@ -109,5 +109,27 @@ describe('SignUp Controller', () => {
     };
     sut.handle(httpRequest);
     expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com');
+  });
+  test('Should return 500 if emailValidator throws', () => {
+    class EmailValidatorStub implements EmailValidator {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      isValid(email: string): boolean {
+        throw new Error();
+      }
+    }
+    const emailValidatorStb = new EmailValidatorStub();
+    const sut = new SignUpController(emailValidatorStb);
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_pass',
+        passwordConfirmation: 'any_pass',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(500);
+    expect(httpResponse.body).toEqual(new ServerError());
   });
 });
