0ab7a18f7e340381fc5173ada654b94a83d2606b
Cristian <cristian123105@gmail.com>
2020-10-06 18:16:41 -0300

    refactor: move encpter creation creation to a factory helper method

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -6,13 +6,18 @@ interface SutTypes {
     encrypterStub: Encrypter
 }
 
-const makeSut = (): SutTypes => {
-    class EncrypterStub{
+const makeEncrypter = (): Encrypter => {
+    class EncrypterStub implements Encrypter{
         async encrypt(value: string): Promise<string> {
             return new Promise(resolve => resolve('hashed_password'))
         }
     }
-    const encrypterStub = new EncrypterStub();
+
+    return new EncrypterStub();
+}
+
+const makeSut = (): SutTypes => {
+    const encrypterStub = makeEncrypter();
     const sut = new DbAddAccount(encrypterStub);
     return {
         sut,
@@ -22,8 +27,8 @@ const makeSut = (): SutTypes => {
 
 describe('Db add account usecase',() => {
     test('Should call encrypter with correct password', async () => {
-        const { sut, encrypterStub } = makeSut()
-        const encryptSpy = jest.spyOn(encrypterStub,'encrypt')
+        const { sut, encrypterStub } = makeSut();
+        const encryptSpy = jest.spyOn(encrypterStub,'encrypt');
         const accountData = {
             name:'valid_name',
             email:'valid_email',
