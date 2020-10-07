fcddc7aa5790ab0f1919d746dc3af796a1c4372a
Cristian <cristian123105@gmail.com>
2020-10-07 16:39:45 -0300

    refactor: add type for salt

--- src/infra/criptography/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -2,7 +2,7 @@ import bcrypt from 'bcrypt';
 import { Encrypter } from '../../data/protocols/encrypter';
 
 export class BcryptAdapter implements Encrypter {
-  private readonly salt;
+  private readonly salt: number;
 
   constructor(salt: number) {
     this.salt = salt;
